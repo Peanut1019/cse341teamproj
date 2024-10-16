bca134f40faa49613d531841c115f651194429b6
Jess Nelson <jemane.19@gmail.com>
2024-10-15 21:10:26 -0600

    W05 partial installs and server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
+const cors = require('cors');
+const port = process.env.PORT || 4040;
+
+app.use(bodyParser.json());
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requestec-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+app.use(cors({methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}));
+app.use(cors({origin: '*'}));
+app.use('/', require('./routes'));
+
