f7ef653eb7965f67f70e6437081c219bcbf04b89
heavenlyAura <goodnessokafor462@gmial.com>
2024-10-23 04:25:04 +0100

    Added PUT and DELETE routes, added more information to the package.json file.

--- controller/grocery.js
+++ controller/grocery.js
@@ -8,7 +8,7 @@ const getAll = async (req, res) => {
     .db("shoppego")
     .collection("grocery")
     .find();
-  result.toArray().then((grocery) => {
+  return result.toArray().then((grocery) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(grocery);
   });
@@ -25,10 +25,80 @@ const getOne = async (req, res) => {
     .db("shoppego")
     .collection("grocery")
     .find({ _id: grocerId });
-  result.toArray().then((grocery) => {
+
+  return result.toArray().then((grocery) => {
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json(grocery[0]);
+    res.status(200).json(grocery);
   });
 };
 
-module.exports = { getAll, getOne };
+const addGrocery = async (req, res) => {
+  // #swagger.tags=["grocery"]
+  try {
+    const db = await mongodb.getDataBase().db("shoppego");
+    const groceryCollection = db.collection("grocery");
+    const { item_name, price, store } = req.body;
+    const newGrocery = {
+      item_name,
+      price,
+      store,
+    };
+    const result = await groceryCollection.insertOne(newGrocery);
+    return res.status(201).json({
+      message: "Grocery item added successfully",
+      data: result.insertedId,
+    });
+  } catch (error) {
+    console.error("Error adding grocery:", error);
+    res.status(500).json({ message: "Failed to add grocery item", error });
+  }
+};
+
+const updateGrocery = async (req, res) => {
+  // #swagger.tags=["grocery"]
+  try {
+    const db = await mongodb.getDataBase().db("shoppego");
+    const groceryCollection = db.collection("grocery");
+    const groceryId = req.params.id;
+    const updatedData = req.body;
+
+    const result = await groceryCollection.updateOne(
+      { _id: new ObjectId(groceryId) },
+      { $set: updatedData }
+    );
+
+    if (result.modifiedCount > 0) {
+      return res.status(200).json({
+        message: `Grocery item with ID ${groceryId} updated successfully`,
+      });
+    } else {
+      res.status(404).json({ message: "Grocery item not found" });
+    }
+  } catch (error) {
+    console.error("Error updating grocery:", error);
+    res.status(500).json({ message: "Failed to update grocery item", error });
+  }
+};
+
+const deleteGrocery = async (req, res) => {
+  // #swagger.tags=["grocery"]
+  try {
+    const db = await mongodb.getDataBase().db("shoppego");
+    const groceryCollection = db.collection("grocery");
+    const groceryId = req.params.id;
+    const result = await groceryCollection.deleteOne({
+      _id: new ObjectId(groceryId),
+    });
+
+    if (result.deletedCount > 0) {
+      res.status(200).json({ message: "Grocery item deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Grocery item not found" });
+    }
+  } catch (error) {
+    console.error("Error deleting grocery:", error);
+    res.status(500).json({ message: "Failed to delete grocery item", error });
+  }
+};
+
+module.exports = { getAll, getOne, addGrocery, updateGrocery, deleteGrocery };
