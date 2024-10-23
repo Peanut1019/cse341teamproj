82947e2631ae1f87e8e32b82537f8162652e63bf
heavenlyAura <goodnessokafor462@gmial.com>
2024-10-23 02:59:41 +0100

    "Formatted code in controller files (grocery.js, library.js, pharmacy.js, users.js) to use consistent spacing and added Swagger tags."

--- controller/library.js
+++ controller/library.js
@@ -1,24 +1,34 @@
-const mongodb = require('../data/database');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../data/database");
+const ObjectId = require("mongodb").ObjectId;
 
-const getAll = async (req, res)=> {
-    const result = await mongodb.getDataBase().db('shoppego').collection('library').find();
-    result.toArray().then((library) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(library);
-    })
-}
+const getAll = async (req, res) => {
+  //#swagger.tags=["library"]
+  const result = await mongodb
+    .getDataBase()
+    .db("shoppego")
+    .collection("library")
+    .find();
+  result.toArray().then((library) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(library);
+  });
+};
 
 const getOne = async (req, res) => {
-    if(!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must be a valid ID to work!');
-    }
-    const libraId = new ObjectId(req.params.id);
-    const result = await mongodb.getDataBase().db('shoppego').collection('library').find({_id: libraId});
-    result.toArray().then((library) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(library[0]);
-    })
-}
+  //#swagger.tags=["library"]
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json("Must be a valid ID to work!");
+  }
+  const libraId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDataBase()
+    .db("shoppego")
+    .collection("library")
+    .find({ _id: libraId });
+  result.toArray().then((library) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(library[0]);
+  });
+};
 
-module.exports = {getAll, getOne}
+module.exports = { getAll, getOne };
--- controller/users.js
+++ controller/users.js
@@ -1,24 +1,34 @@
-const mongodb = require('../data/database');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../data/database");
+const ObjectId = require("mongodb").ObjectId;
 
-const getAll = async (req, res)=> {
-    const reesult = await mongodb.getDataBase().db('shoppego').collection('users').find();
-    reesult.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    })
-}
+const getAll = async (req, res) => {
+  //#swagger.tags=["users"]
+  const reesult = await mongodb
+    .getDataBase()
+    .db("shoppego")
+    .collection("users")
+    .find();
+  reesult.toArray().then((users) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(users);
+  });
+};
 
 const getOne = async (req, res) => {
-    if(!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must be a valid ID to work!');
-    }
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDataBase().db('shoppego').collection('users').find({_id: userId});
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
-    })
-}
+  //#swagger.tags=["users"]
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json("Must be a valid ID to work!");
+  }
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDataBase()
+    .db("shoppego")
+    .collection("users")
+    .find({ _id: userId });
+  result.toArray().then((users) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(users[0]);
+  });
+};
 
-module.exports = {getAll, getOne}
+module.exports = { getAll, getOne };
